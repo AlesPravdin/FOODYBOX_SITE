afabdf86af0b00eee40b967282c73232526dc2bb
Ales Pravdin <pravdin.ales@gmail.com>
2017-12-18 12:14:27 +0300

    Update functions.sql

--- sql/functions.sql
+++ sql/functions.sql
@@ -17,3 +17,31 @@ BEGIN
 END $$
 
 DELIMITER ;
+
+
+
+DELIMITER $$
+
+CREATE PROCEDURE `report_dates`(
+  IN fromDate DATE,
+  IN toDate   DATE
+)
+  BEGIN
+
+    SELECT
+      P.product_id,
+      P.name,
+      P.model,
+      OPT.value                            AS color,
+      SUM(P.quantity)                      AS quantity,
+      SUM(P.total + P.total * P.tax / 100) AS total
+    FROM `oc_order_product` P
+      LEFT JOIN `oc_order` O ON (P.order_id = O.order_id)
+      LEFT JOIN `oc_order_option` OPT ON (P.order_product_id = OPT.order_product_id)
+    WHERE (O.order_status_id > '0') AND (O.date_added BETWEEN fromDate AND toDate)
+    GROUP BY P.product_id, color
+    ORDER BY name;
+
+  END $$
+
+DELIMITER ;
